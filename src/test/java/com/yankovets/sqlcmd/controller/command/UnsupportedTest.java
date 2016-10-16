7728f50d8b0891b3055eb98ea20c548422eb1de1
ArtemYankovets <ArtemYankovets@gmail.com>
2016-10-16 13:13:36 +0300

    Did 100% test coverage on Unsupported command

---
+++ src/test/java/com/yankovets/sqlcmd/controller/command/UnsupportedTest.java
@@ -0,0 +1,40 @@
+package com.yankovets.sqlcmd.controller.command;
+
+import com.yankovets.sqlcmd.view.View;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class UnsupportedTest {
+
+    private View view;
+    private Command command;
+
+    @Before
+    public void setUp() {
+        view = mock(View.class);
+        command = new Unsupported(view);
+    }
+
+    @Test
+    public void testCanProcessUnsupportedString() {
+        // when
+        boolean canProcess = command.canProcess(anyString());
+
+        // then
+        assertTrue(canProcess);
+    }
+
+    @Test
+    public void testCanProcessNotExist() {
+        // when
+        command.process("'some wrong command'");
+
+        // then
+        verify(view).write("Non exist command: 'some wrong command'");
+    }
+}
