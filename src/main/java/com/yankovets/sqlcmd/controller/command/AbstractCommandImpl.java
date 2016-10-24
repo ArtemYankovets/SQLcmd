e53dc2da59c3294c49472fdaa021a0a2969e218d
ArtemYankovets <ArtemYankovets@gmail.com>
2016-10-24 18:44:15 +0300

    Changing the project architecture. Created new abstract class which implements interface Command and include all common functional from all commands

---
+++ src/main/java/com/yankovets/sqlcmd/controller/command/AbstractCommandImpl.java
@@ -0,0 +1,28 @@
+package com.yankovets.sqlcmd.controller.command;
+
+import com.yankovets.sqlcmd.controller.command.util.CommandValidation;
+import com.yankovets.sqlcmd.model.DatabaseManager;
+import com.yankovets.sqlcmd.view.View;
+
+public abstract class AbstractCommandImpl implements Command {
+    protected DatabaseManager manager;
+    protected View view;
+    protected CommandValidation validator;
+
+    public AbstractCommandImpl(View view) {
+        this.view = view;
+    }
+
+    public AbstractCommandImpl (DatabaseManager databaseManager, View view) {
+        this.manager = databaseManager;
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        validator = new CommandValidation(command, getCommandTemplate());
+        String[] paramOfCommandTemplate = validator.splitCommandUpOnParameters(getCommandTemplate());
+        String commandLineStartWith = validator.getParametersOfCommandLine()[0];
+        return paramOfCommandTemplate[0].equals(commandLineStartWith);
+    }
+}
