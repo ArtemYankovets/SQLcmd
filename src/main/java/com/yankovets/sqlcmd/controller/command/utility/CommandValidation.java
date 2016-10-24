0c74bfc3f8e294272169ec201a5bcb797255a72c
ArtemYankovets <ArtemYankovets@gmail.com>
2016-10-24 18:48:17 +0300

    Add new utility class. It helps to validate the input line like a command with parameters

---
+++ src/main/java/com/yankovets/sqlcmd/controller/command/utility/CommandValidation.java
@@ -0,0 +1,46 @@
+package com.yankovets.sqlcmd.controller.command.utility;
+
+public class CommandValidation {
+
+    private String commandTemplate;
+    private String commandLine;
+    private final String[] PARAMETERS_OF_COMMAND_LINE;
+
+    public CommandValidation(String commandLine, String commandTemplate) {
+        this.commandLine = commandLine;
+        this.commandTemplate = commandTemplate;
+        PARAMETERS_OF_COMMAND_LINE = splitCommandUpOnParameters(commandLine);
+    }
+
+    public void validate() {
+
+        commandFormatValidation();
+    }
+
+    private void commandFormatValidation() {
+        String[] paramOfCommandTemplate = splitCommandUpOnParameters(commandTemplate);
+
+        if (paramOfCommandTemplate.length != PARAMETERS_OF_COMMAND_LINE.length) {
+            throw new IllegalArgumentException(String.format("The amount of parameters for this command," +
+                            " which split by '|' are %s, but expected %s",
+                    paramOfCommandTemplate.length, PARAMETERS_OF_COMMAND_LINE.length));
+            // A number of parameters is ...
+        }
+    }
+
+    public String[] splitCommandUpOnParameters(String string){
+        return string.split("[|]");
+    }
+
+    public void checkNumberOfParameters() {
+        if (getParametersOfCommandLine().length % 2 != 0) {
+            throw new IllegalArgumentException(String.format("Must be even number of parameters " +
+                    "in format 'createEntry|tableName|column1|value1|column2|value2|...|columnN|valueN', " +
+                    "you typed: '%s'", commandLine));
+        }
+    }
+
+    public String[] getParametersOfCommandLine() {
+        return PARAMETERS_OF_COMMAND_LINE;
+    }
+}
